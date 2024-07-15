bc3c8953022f170fb5bb51c2fe3b02e044a5db58
254dennis <info.dennismwangi@gmail.com>
2024-07-15 19:04:30 +0300

    changes

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-$(function(){
-    $("#js-shopping-list-form").submit(function(event) {
-      event.preventDefault();
-    const listItem = $("#shopping-list-entry")();
-    $("#shopping-list-entry").val(" ");
-    $(".shopping-list").append(
-        `<li>
-          <span class="shopping-item">${listItem}</span>
-          <div class="shopping-item-controls">
-            <button class="shopping-item-toggle">
-              <span class="button-label">check</span>
-            </button>
-            <button class="shopping-item-delete">
-              <span class="button-label">delete</span>
-            </button>
-          </div>
-        </li>`);
-  });
-  
-  $(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
-    $(this).closest("li").find(".shopping-item").toggle);
-  });
-  
-  function(append.listItem) 
-    $(this).closest("li").remove();
-  
-  
-  
+window.addEventListener('load', ()=> {
+  const form = document.querySelector("listform");
+  const input = document.querySelector("shopping-list-entry")
+  const list_el = document.querySelector ("")
+})
